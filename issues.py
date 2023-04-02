044f3227331cae06b8ba4a35ae25482feab6c6d0
Eric Melz <emelz@gumgum.com>
2023-04-02 11:42:10 -0700

    progress

--- issues.py
+++ issues.py
@@ -3,6 +3,8 @@ import urllib3
 import json
 import os
 
+from enum import Enum
+
 PROJECT_ID = '15836'
 # PROJECT_KEY = 'VE'
 PROJECT_KEY = 'VBI'
@@ -12,6 +14,18 @@ SPRINT_URL = 'https://gumgum.jira.com/rest/agile/1.0/sprint'
 ISSUE_ID = '457'
 SPRINT_ID = '1010'
 ISSUE_LIMIT = 1000
+
+
+class Enter(Enum):
+    PLANNED = 1
+    UNPLANNED = 2
+
+
+class Exit(Enum):
+    COMPLETED = 1
+    NOT_COMPLETED = 2
+    PUSHED = 3
+    COMPLETED_EARLY = 4
 
 
 def get_issue(issue_id):
@@ -36,15 +50,18 @@ def is_subtask(issue):
     return issue['fields']['issuetype']['name'] == 'Sub-task'
 
 
-def is_issue_finished(sprint_id, issue):
+def issue_finished(sprint_id, issue):
     # if issue['key'] == 'VBI-480':
     #     print('*****FOUND!')
+    sprint_start_timestamp = None
     sprint_close_timestamp = None
     for closed_sprint in issue['fields']['closedSprints']:
         if int(sprint_id) == closed_sprint['id']:
+            sprint_start_timestamp = dateutil.parser.isoparse(closed_sprint['startDate'])
             sprint_close_timestamp = dateutil.parser.isoparse(closed_sprint['completeDate'])
     finished = False
     finished_timestamp = None
+    result = Exit.NOT_COMPLETED
     for entry in issue['changelog']['histories']:
         entry_timestamp = dateutil.parser.isoparse(entry['created'])
         for item in entry['items']:
@@ -55,6 +72,10 @@ def is_issue_finished(sprint_id, issue):
                         finished_timestamp = entry_timestamp
                     elif not finished:
                         finished = True
+                        if entry_timestamp < sprint_start_timestamp:
+                            result = Exit.COMPLETED_EARLY
+                        else:
+                            result = Exit.COMPLETED
                         finished_timestamp = entry_timestamp
 
     # undo finished if the item was reopened
@@ -66,18 +87,19 @@ def is_issue_finished(sprint_id, issue):
                 if finished and item['fromString'] == 'Closed':
                     if entry_timestamp > finished_timestamp:
                         finished = False
-    return finished
+                        result = Exit.NOT_COMPLETED
+    return result
 
 
 class Issue:
-    def __init__(self, key, planned, finished, subtask):
+    def __init__(self, key, planned, exit, subtask):
         self.key = key
         self.planned = planned
-        self.finished = finished
+        self.exit = exit
         self.subtask = subtask
 
     def __str__(self):
-        return f'<{self.key} planned={self.planned} finished={self.finished} subtask={self.subtask}>'
+        return f'<{self.key} planned={self.planned} exit={self.exit} subtask={self.subtask}>'
 
 
 def get_sprint_issues(sprint_id):
@@ -94,19 +116,25 @@ def get_sprint_issues(sprint_id):
     result = []
     for issue in data['issues']:
         planned = is_issue_planned(issue)
-        finished = is_issue_finished(sprint_id, issue)
+        finished = issue_finished(sprint_id, issue)
         subtask = is_subtask(issue)
         result.append(Issue(issue['key'], planned, finished, subtask))
     return result
 
 
+def sortable_issue_key(issue):
+    pieces = issue.key.split('-')
+    return pieces[0], int(pieces[1])
+
+
 def print_issues(issues):
-    print('Completed:')
-    for issue in [x for x in sorted(issues, key=lambda x: x.key) if x.finished and not x.subtask]:
-        print(issue)
-    print('\nNot Completed:')
-    for issue in [x for x in sorted(issues, key=lambda x: x.key) if not x.finished and not x.subtask]:
-        print(issue)
+    for label, exit in [('Completed', Exit.COMPLETED),
+                        ('Not Completed', Exit.NOT_COMPLETED),
+                        ('Completed Early', Exit.COMPLETED_EARLY)]:
+        print(f'\n{label}:')
+        for issue in [x for x in sorted(issues, key=sortable_issue_key)
+                      if x.exit == exit and not x.subtask]:
+            print(issue)
 
 
 def main():
