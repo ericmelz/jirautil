c32bbd16c73c407c80ba3659dd8f03b33e252319
Eric Melz <emelz@gumgum.com>
2023-04-07 12:05:07 -0700

    completed confluence tutorial

--- forge_tutorial/hello-world-app/src/index.jsx
+++ forge_tutorial/hello-world-app/src/index.jsx
@@ -1,15 +1,32 @@
-import ForgeUI, { render, Fragment, Macro, Text } from "@forge/ui";
+import api, { route } from "@forge/api";
+import ForgeUI, { render, Fragment, Macro, Text, useProductContext, useState } from "@forge/ui";
+
+const fetchCommentsForContent = async (contentId) => {
+  const res = await api
+    .asUser()
+    .requestConfluence(route`/wiki/rest/api/content/${contentId}/child/comment`);
+
+  const data = await res.json();
+  return data.results;
+};
 
 const App = () => {
-  return (
-    <Fragment>
-      <Text>yowza world!</Text>
-    </Fragment>
-  );
+    const context = useProductContext();
+    const [comments] = useState(async () => await fetchCommentsForContent(context.contentId));
+
+    console.log(`Number of comments on this page: ${comments.length}`);
+    return (
+	<Fragment>
+	    <Text>
+		Number of comments on this page: {comments.length}
+	    </Text>
+	    <Text>yowza world!</Text>
+	</Fragment>
+    );
 };
 
 export const run = render(
-  <Macro
-    app={<App />}
-  />
+    <Macro
+	app={<App />}
+    />
 );
